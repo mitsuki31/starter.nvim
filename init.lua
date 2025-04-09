5dadc286f516f8c5bad7c1c3c8b587804f5174f5
Ryuu Mitsuki <dhefam31@gmail.com>
2025-04-09 14:29:04 +0700

    modify nvim initializer

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.cmp")
+require("config.renamer")
+
+vim.cmd('colorscheme nightfly')
+
+-- Disable plugins check order
+vim.g.lazyvim_check_order = false
