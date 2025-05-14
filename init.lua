9b8cff44b6335b0b0a9e68562d978648607c24b0
Ryuu Mitsuki <dhefam31@gmail.com>
2025-05-14 11:52:06 +0700

    add some dependencies

--- init.lua
+++ init.lua
@@ -2,8 +2,11 @@
 require("config.lazy")
 require("config.cmp")
 require("config.renamer")
+require("config.indent-blankline")
+require("config.rainbow-delimiters")
 
-vim.cmd('colorscheme nightfly')
+vim.cmd("colorscheme nightfly")
+vim.g.nightflyTransparent = true
 
 -- Disable plugins check order
 vim.g.lazyvim_check_order = false
